3aa7bcbf3c33a58e03d2bacb5f3974d8bef4c5f9
Niyaz <niazsajdullin@gmail.com>
2025-08-26 15:01:47 +0500

    ADD: constructors to MapperTo

--- src/main/java/com/vunnen/bellintegrator/dto/MessageTo.java
+++ src/main/java/com/vunnen/bellintegrator/dto/MessageTo.java
@@ -1,12 +1,12 @@
 package com.vunnen.bellintegrator.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class MessageTo {
     private String msg_id;
     private String timestamp;
