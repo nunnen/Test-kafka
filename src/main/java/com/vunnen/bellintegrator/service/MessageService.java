1e45f0e203c8b1227e3071dcf15b744cb1574013
Niyaz <niazsajdullin@gmail.com>
2025-08-26 15:02:27 +0500

    ADD: kafka message consumer & logging

--- src/main/java/com/vunnen/bellintegrator/service/MessageService.java
+++ src/main/java/com/vunnen/bellintegrator/service/MessageService.java
@@ -2,18 +2,25 @@ package com.vunnen.bellintegrator.service;
 
 import com.vunnen.bellintegrator.dto.MessageIn;
 import com.vunnen.bellintegrator.dto.MessageTo;
-import com.vunnen.bellintegrator.producer.MessageProducer;
+import com.vunnen.bellintegrator.model.Message;
+import com.vunnen.bellintegrator.model.MessageMapper;
+import com.vunnen.bellintegrator.kafka.MessageProducer;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MessageService {
     private final MessageProducer producer;
+    private final MessageMapper mapper;
 
     public MessageTo getMessageTo(MessageIn messageIn) {
+        log.info("Mapping message-{} from messageIn to messageTo", messageIn.getMsg_id());
         return MessageTo.builder()
                 .msg_id(String.valueOf(messageIn.getMsg_id()))
                 .timestamp(String.valueOf(Instant.now().toEpochMilli()))
@@ -22,7 +29,14 @@ public class MessageService {
                 .build();
     }
 
+    @KafkaListener(topics = "${kafka.topic.name}", groupId = "messages")
+    public void listen(MessageTo messageTo) {
+        Message message = mapper.mapTo(messageTo);
+        log.info("Listening message-{}", message);
+    }
+
     public void sendMessage(MessageTo messageTo) {
+        log.info("Sending message-{} to kafka", messageTo.getMsg_id());
         producer.sendMessage(messageTo);
     }
 
