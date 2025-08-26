21f053cb849b6e5e18a916a122194e703617a8b8
Niyaz <niazsajdullin@gmail.com>
2025-08-26 15:02:02 +0500

    ADD: logging info

--- src/main/java/com/vunnen/bellintegrator/kafka/MessageProducer.java
+++ src/main/java/com/vunnen/bellintegrator/kafka/MessageProducer.java
@@ -1,19 +1,22 @@
-package com.vunnen.bellintegrator.producer;
+package com.vunnen.bellintegrator.kafka;
 
 import com.vunnen.bellintegrator.dto.MessageTo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MessageProducer {
     private final KafkaTemplate<String, MessageTo> kafkaTemplate;
     @Value("${kafka.topic.name}")
     private String topic;
 
     public void sendMessage(MessageTo messageTo) {
+        log.info("Sending Message to topic: " + topic);
         kafkaTemplate.send(topic, messageTo);
     }
 }
--- src/main/java/com/vunnen/bellintegrator/model/MessageMapper.java
+++ src/main/java/com/vunnen/bellintegrator/model/MessageMapper.java
@@ -1,12 +1,15 @@
 package com.vunnen.bellintegrator.model;
 
 import com.vunnen.bellintegrator.dto.MessageTo;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class MessageMapper {
 
     public Message mapTo(MessageTo messageTo) {
+        log.info("MessageMapper mapping messageTo-{} to message", messageTo.getMsg_id());
         Message message = new Message();
         message.setMsg_id(Long.parseLong(messageTo.getMsg_id()));
         message.setTimestamp(Long.parseLong(messageTo.getTimestamp()));
